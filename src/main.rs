6962c1990ea7ef740411c7abab94717f2dcd33ab
Mike Dilger <mike@optcomp.nz>
2020-06-09 18:30:34 +1200

    Update VP9 (google rec'd, two-pass, CQ mode); increase cpulimit

--- src/main.rs
+++ src/main.rs
@@ -16,9 +16,149 @@ use loudnorm::*;
 const CPULIMIT_PATH: &'static str = "/usr/bin/cpulimit";
 const FFMPEG_PATH: &'static str = "/usr/bin/ffmpeg";
 
-const CPULIMIT: &'static str = "600";
+const CPULIMIT: &'static str = "1200";
 
 const USAGE: &'static str = "USAGE: prepvideo <inputfile> <title> <1920|1280|1024|854|640>";
+
+#[derive(Debug, Clone, Copy)]
+struct Vp9Params {
+    scale: &'static str,
+    target: u32,
+    crf: u32,
+    pass1speed: u32,
+    pass2speed: u32,
+    tile_columns: u32,
+    threads: u32,
+}
+
+// Google recommendations for Video on Demand (file based):
+// https://web.archive.org/web/20200117200622/https://developers.google.com/media/vp9/settings/vod/
+// https://developers.google.com/media/vp9/settings/vod/
+/*
+const VP9_PARAMS_320_240: Vp9Params = Vp9Params {
+    scale: "320x240",
+    target: 150,
+    crf: 37,
+    pass1speed: 4,
+    pass2speed: 1,
+    tile_columns: 0,
+    threads: 2,
+};
+*/
+const VP9_PARAMS_640_360: Vp9Params = Vp9Params {
+    scale: "640x360",
+    target: 276,
+    crf: 36,
+    pass1speed: 4,
+    pass2speed: 1,
+    tile_columns: 1,
+    threads: 4,
+};
+/*
+const VP9_PARAMS_640_480: Vp9Params = Vp9Params {
+    scale: "640x480",
+    target: 631, // half way between 512(LQ) and 750(MQ)
+    crf: 33, // chosen from MQ
+    pass1speed: 4,
+    pass2speed: 1,
+    tile_columns: 1,
+    threads: 4,
+};
+*/
+const VP9_PARAMS_854_480: Vp9Params = Vp9Params { // I estimated this one
+    scale: "854x480",
+    target: 750, // half way between 512(LQ) and 750(MQ)
+    crf: 33, // chosen from MQ
+    pass1speed: 4,
+    pass2speed: 1,
+    tile_columns: 1,
+    threads: 4,
+};
+const VP9_PARAMS_1024_576: Vp9Params = Vp9Params { // I estimated this one
+    scale: "1024x576",
+    target: 820,
+    crf: 32,
+    pass1speed: 4,
+    pass2speed: 2,
+    tile_columns: 2,
+    threads: 8,
+};
+const VP9_PARAMS_1280_720: Vp9Params = Vp9Params {
+    scale: "1280x720",
+    target: 1024,
+    crf: 32,
+    pass1speed: 4,
+    pass2speed: 2,
+    tile_columns: 2,
+    threads: 8,
+};
+/*
+const VP9_PARAMS_1280_720_60HZ: Vp9Params = Vp9Params {
+    scale: "1280x720",
+    target: 1800,
+    crf: 32,
+    pass1speed: 4,
+    pass2speed: 2,
+    tile_columns: 2,
+    threads: 8,
+};
+*/
+const VP9_PARAMS_1920_1080: Vp9Params = Vp9Params {
+    scale: "1920x1080",
+    target: 1800,
+    crf: 31,
+    pass1speed: 4,
+    pass2speed: 2,
+    tile_columns: 2,
+    threads: 8,
+};
+/*
+const VP9_PARAMS_1920_1080_60HZ: Vp9Params = Vp9Params {
+    scale: "1920x1080",
+    target: 3000,
+    crf: 31,
+    pass1speed: 4,
+    pass2speed: 2,
+    tile_columns: 2,
+    threads: 8,
+};
+const VP9_PARAMS_2560_1440: Vp9Params = Vp9Params {
+    scale: "2560x1440",
+    target: 6000,
+    crf: 24,
+    pass1speed: 4,
+    pass2speed: 2,
+    tile_columns: 3,
+    threads: 16,
+};
+const VP9_PARAMS_2560_1440_60HZ: Vp9Params = Vp9Params {
+    scale: "2560x1440",
+    target: 9000,
+    crf: 31,
+    pass1speed: 4,
+    pass2speed: 2,
+    tile_columns: 3,
+    threads: 16,
+};
+const VP9_PARAMS_3840_2160: Vp9Params = Vp9Params {
+    scale: "3840x2160",
+    target: 12000,
+    crf: 15,
+    pass1speed: 4,
+    pass2speed: 2,
+    tile_columns: 3,
+    threads: 16,
+};
+const VP9_PARAMS_3840_2160_60HZ: Vp9Params = Vp9Params {
+    scale: "3840x2160",
+    target: 18000,
+    crf: 15,
+    pass1speed: 4,
+    pass2speed: 2,
+    tile_columns: 3,
+    threads: 16,
+};
+ */
 
 fn args_shrink<'a>(command: &mut Command, size: &str) {
     command
@@ -26,26 +166,58 @@ fn args_shrink<'a>(command: &mut Command, size: &str) {
         .arg(&*format!("scale={}", size));
 }
 
-fn args_vp9<'a>(command: &mut Command, quality: u8) {
+fn args_vp9<'a>(command: &mut Command, vp9params: Vp9Params) {
     command
+        .arg("-b:v").arg(&*format!("{}", vp9params.target))
+        .arg("-minrate").arg(&*format!("{}", vp9params.target * 50 / 100))
+        .arg("-maxrate").arg(&*format!("{}", vp9params.target * 145 / 100))
+        .arg("-tile-columns").arg(&*format!("{}", vp9params.tile_columns))
+        .arg("-g").arg("240")        // keyframe spacing
+        .arg("-threads").arg(&*format!("{}", vp9params.threads))
+        .arg("-quality").arg("good")
+        .arg("-crf").arg(&*format!("{}", vp9params.crf))
         .arg("-c:v").arg("libvpx-vp9")
-        .arg("-crf").arg(&*format!("{}", quality))
-        .arg("-b:v").arg("0")
         .arg("-c:a").arg("libopus");
 }
 
-fn convert(input: &str, output: &str, loudnorm: &Loudnorm, size: &str, quality: u8) {
+fn convert(input: &str, outputfile: &str, loudnorm: &Loudnorm, vp9params: Vp9Params) {
+    // Pass 1
     let mut command = Command::new(CPULIMIT_PATH);
     command.arg("-l").arg(CPULIMIT)
         .arg(FFMPEG_PATH)
-        .arg("-y")
+        .arg("-i").arg(input); // skip loudnorm on pass1
+    args_shrink(&mut command, vp9params.scale);
+    args_vp9(&mut command, vp9params);
+    command.arg("-pass").arg("1")
+        .arg("-speed").arg(&*format!("{}", vp9params.pass1speed))
+        .arg("-y");
+    command.arg(outputfile);
+
+    println!("{:?}", command);
+
+    let output = command
+        .output()
+        .expect("failed to execute ffmpeg");
+
+    if ! output.status.success() {
+        let stderr_str = String::from_utf8_lossy(&*output.stderr).to_string();
+        panic!("Failed to run ffmpeg multi command.  Stderr follows.\n{}",
+               stderr_str);
+    }
+
+    // Pass 2
+    let mut command = Command::new(CPULIMIT_PATH);
+    command.arg("-l").arg(CPULIMIT)
+        .arg(FFMPEG_PATH)
         .arg("-i").arg(input)
         .arg("-af")
         .arg(&*loudnorm.convert_af());
-
-    args_vp9(&mut command, quality);
-    args_shrink(&mut command, size);
-    command.arg(output);
+    args_shrink(&mut command, vp9params.scale);
+    args_vp9(&mut command, vp9params);
+    command.arg("-pass").arg("2")
+        .arg("-speed").arg(&*format!("{}", vp9params.pass2speed))
+        .arg("-y");
+    command.arg(outputfile);
 
     println!("{:?}", command);
 
@@ -141,21 +313,12 @@ fn main() {
     let title = args.next().expect(USAGE);
     let resolution = args.next().expect(USAGE);
 
-    // vp9 quality 0-63, recommended 15-35, with 31 recd for 1080p HD
-    // See https://developers.google.com/media/vp9/settings/vod/
-    let (size, quality) = if &*resolution == "1920" {
-        ("1920:1080", 31)
-    } else if &*resolution == "1280" {
-        ("1280:720", 32)
-    } else if &*resolution == "1024" {
-        ("1024:576", 35)
-    } else if &*resolution == "854" {
-        ("854:480", 36)
-    } else if &*resolution == "640" {
-        ("640:360", 36)
-    } else {
-        panic!(USAGE)
-    };
+    let vp9params = if &*resolution == "1920" { VP9_PARAMS_1920_1080 }
+    else if &*resolution == "1280" { VP9_PARAMS_1280_720 }
+    else if &*resolution == "1024" { VP9_PARAMS_1024_576 }
+    else if &*resolution == "854" { VP9_PARAMS_854_480 }
+    else if &*resolution == "640" { VP9_PARAMS_640_360 }
+    else {  panic!(USAGE) };
 
     // Concat all mp4 files in current directory if inputfile was "."
     if input_file == "." {
@@ -168,8 +331,8 @@ fn main() {
     println!("Analyzing loudnorm...");
     let loudnorm = Loudnorm::from_analyze(&input_file);
 
-    println!("Converting video (second pass w/ multiple functions)...");
-    convert(&input_file, "tmp1.webm", &loudnorm, size, quality);
+    println!("Converting video (two passes w/ multiple functions)...");
+    convert(&input_file, "tmp1.webm", &loudnorm, vp9params);
 
     let output_name = title
         .replace("/", "-")
