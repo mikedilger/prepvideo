749007e2d6510fdd9014594cec6d7df38047ddb2
Mike Dilger <mike@optcomp.nz>
2020-06-12 17:32:22 +1200

    Simplied VP9 parameters (near average of google recommendation)

--- src/main.rs
+++ src/main.rs
@@ -15,21 +15,11 @@ use loudnorm::*;
 
 const CPULIMIT_PATH: &'static str = "/usr/bin/cpulimit";
 const FFMPEG_PATH: &'static str = "/usr/bin/ffmpeg";
+const FFPROBE_PATH: &'static str = "/usr/bin/ffprobe";
 
 const CPULIMIT: &'static str = "1200";
 
-const USAGE: &'static str = "USAGE: prepvideo <inputfile> <title> <1920|1280|1024|854|640>";
-
-#[derive(Debug, Clone, Copy)]
-struct Vp9Params {
-    scale: &'static str,
-    target: u32, // in kbps
-    crf: u32,
-    pass1speed: u32,
-    pass2speed: u32,
-    tile_columns: u32,
-    threads: u32,
-}
+const USAGE: &'static str = "USAGE: prepvideo <inputfile> <title> <x-resolution>";
 
 // AS A REFERENCE POINT, my Nexus 5x phone video comes out in H.264 at 1920x1200 @30hz
 // w/ a bit rate of 16,995 kbps.  That's 9.44x times as many bits as google's VOD recommendation
@@ -46,131 +36,6 @@ struct Vp9Params {
 // Google recommendations for Video on Demand (file based):
 // https://web.archive.org/web/20200117200622/https://developers.google.com/media/vp9/settings/vod/
 // https://developers.google.com/media/vp9/settings/vod/
-/*
-const VP9_PARAMS_320_240: Vp9Params = Vp9Params {
-    scale: "320x240",
-    target: 150,
-    crf: 37,
-    pass1speed: 4,
-    pass2speed: 1,
-    tile_columns: 0,
-    threads: 2,
-};
-*/
-const VP9_PARAMS_640_360: Vp9Params = Vp9Params {
-    scale: "640x360",
-    target: 276,
-    crf: 36,
-    pass1speed: 4,
-    pass2speed: 1,
-    tile_columns: 1,
-    threads: 4,
-};
-/*
-const VP9_PARAMS_640_480: Vp9Params = Vp9Params {
-    scale: "640x480",
-    target: 631, // half way between 512(LQ) and 750(MQ)
-    crf: 33, // chosen from MQ
-    pass1speed: 4,
-    pass2speed: 1,
-    tile_columns: 1,
-    threads: 4,
-};
-*/
-const VP9_PARAMS_854_480: Vp9Params = Vp9Params { // I estimated this one
-    scale: "854x480",
-    target: 750, // half way between 512(LQ) and 750(MQ)
-    crf: 33, // chosen from MQ
-    pass1speed: 4,
-    pass2speed: 1,
-    tile_columns: 1,
-    threads: 4,
-};
-const VP9_PARAMS_1024_576: Vp9Params = Vp9Params { // I estimated this one
-    scale: "1024x576",
-    target: 820,
-    crf: 32,
-    pass1speed: 4,
-    pass2speed: 2,
-    tile_columns: 2,
-    threads: 8,
-};
-const VP9_PARAMS_1280_720: Vp9Params = Vp9Params {
-    scale: "1280x720",
-    target: 1024,
-    crf: 32,
-    pass1speed: 4,
-    pass2speed: 2,
-    tile_columns: 2,
-    threads: 8,
-};
-/*
-const VP9_PARAMS_1280_720_60HZ: Vp9Params = Vp9Params {
-    scale: "1280x720",
-    target: 1800,
-    crf: 32,
-    pass1speed: 4,
-    pass2speed: 2,
-    tile_columns: 2,
-    threads: 8,
-};
-*/
-const VP9_PARAMS_1920_1080: Vp9Params = Vp9Params {
-    scale: "1920x1080",
-    target: 1800,
-    crf: 31,
-    pass1speed: 4,
-    pass2speed: 2,
-    tile_columns: 2,
-    threads: 8,
-};
-/*
-const VP9_PARAMS_1920_1080_60HZ: Vp9Params = Vp9Params {
-    scale: "1920x1080",
-    target: 3000,
-    crf: 31,
-    pass1speed: 4,
-    pass2speed: 2,
-    tile_columns: 2,
-    threads: 8,
-};
-const VP9_PARAMS_2560_1440: Vp9Params = Vp9Params {
-    scale: "2560x1440",
-    target: 6000,
-    crf: 24,
-    pass1speed: 4,
-    pass2speed: 2,
-    tile_columns: 3,
-    threads: 16,
-};
-const VP9_PARAMS_2560_1440_60HZ: Vp9Params = Vp9Params {
-    scale: "2560x1440",
-    target: 9000,
-    crf: 31,
-    pass1speed: 4,
-    pass2speed: 2,
-    tile_columns: 3,
-    threads: 16,
-};
-const VP9_PARAMS_3840_2160: Vp9Params = Vp9Params {
-    scale: "3840x2160",
-    target: 12000,
-    crf: 15,
-    pass1speed: 4,
-    pass2speed: 2,
-    tile_columns: 3,
-    threads: 16,
-};
-const VP9_PARAMS_3840_2160_60HZ: Vp9Params = Vp9Params {
-    scale: "3840x2160",
-    target: 18000,
-    crf: 15,
-    pass1speed: 4,
-    pass2speed: 2,
-    tile_columns: 3,
-    threads: 16,
-};
- */
 
 fn args_shrink<'a>(command: &mut Command, size: &str) {
     command
@@ -178,35 +43,49 @@ fn args_shrink<'a>(command: &mut Command, size: &str) {
         .arg(&*format!("scale={}", size));
 }
 
-fn args_vp9<'a>(command: &mut Command, vp9params: Vp9Params) {
-    // We only need 70% of the target Google recommends.
-    // I can't see any significant difference.
-    // (Our old method was only 66% as many bits, and it looked good to me)
-    let bitrate = vp9params.target * 700 / 1000;
+fn args_vp9<'a>(command: &mut Command, xres: i32, yres: i32, fps: i32) {
+    let uncompressed_bitrate = 24 * fps * xres * yres;
+    let vp9_bitrate = uncompressed_bitrate / 640; // 640 is near average of google recommendations
+    println!("VP9 bitrate will be {}kbps", vp9_bitrate / 1000);
+
+    let tile_columns = if xres < 640 { 0 }
+    else if xres < 1024 { 1 }
+    else if xres < 2560 { 2 }
+    else { 3 };
+
+    let threads = 16; // for me, always use 16 threads
+    let crf = 31; // 31 is always reasonable
 
     command
-        .arg("-b:v").arg(&*format!("{}k", bitrate))
-        .arg("-minrate").arg(&*format!("{}k", bitrate * 50 / 100))
-        .arg("-maxrate").arg(&*format!("{}k", bitrate * 145 / 100))
-        .arg("-tile-columns").arg(&*format!("{}", vp9params.tile_columns))
+        .arg("-b:v").arg(&*format!("{}", vp9_bitrate))
+        .arg("-minrate").arg(&*format!("{}", vp9_bitrate * 50 / 100))
+        .arg("-maxrate").arg(&*format!("{}", vp9_bitrate * 145 / 100))
+        .arg("-tile-columns").arg(&*format!("{}", tile_columns))
         .arg("-g").arg("240")        // keyframe spacing
-        .arg("-threads").arg(&*format!("{}", vp9params.threads))
+        .arg("-threads").arg(&*format!("{}", threads))
         .arg("-quality").arg("good")
-        .arg("-crf").arg(&*format!("{}", vp9params.crf))
+        .arg("-crf").arg(&*format!("{}", crf))
         .arg("-c:v").arg("libvpx-vp9")
         .arg("-c:a").arg("libopus");
 }
 
-fn convert(input: &str, outputfile: &str, loudnorm: &Loudnorm, vp9params: Vp9Params) {
+fn convert(input: &str, outputfile: &str, loudnorm: &Loudnorm, xres: i32, fps: i32) {
+
+    // We always use 1:1.777777 aspect ratios
+    let yres = xres * 5625 / 10000;
+
+    let pass1speed = 4; // at all resolutions
+    let pass2speed = if xres < 1024 { 1 } else { 2 };
+
     // Pass 1
     let mut command = Command::new(CPULIMIT_PATH);
     command.arg("-l").arg(CPULIMIT)
         .arg(FFMPEG_PATH)
         .arg("-i").arg(input); // skip loudnorm on pass1
-    args_shrink(&mut command, vp9params.scale);
-    args_vp9(&mut command, vp9params);
+    args_shrink(&mut command, &*format!("{}x{}", xres, yres));
+    args_vp9(&mut command, xres, yres, fps);
     command.arg("-pass").arg("1")
-        .arg("-speed").arg(&*format!("{}", vp9params.pass1speed))
+        .arg("-speed").arg(&*format!("{}", pass1speed))
         .arg("-y");
     command.arg(outputfile);
 
@@ -229,10 +108,10 @@ fn convert(input: &str, outputfile: &str, loudnorm: &Loudnorm, vp9params: Vp9Par
         .arg("-i").arg(input)
         .arg("-af")
         .arg(&*loudnorm.convert_af());
-    args_shrink(&mut command, vp9params.scale);
-    args_vp9(&mut command, vp9params);
+    args_shrink(&mut command, &*format!("{}x{}", xres, yres));
+    args_vp9(&mut command, xres, yres, fps);
     command.arg("-pass").arg("2")
-        .arg("-speed").arg(&*format!("{}", vp9params.pass2speed))
+        .arg("-speed").arg(&*format!("{}", pass2speed))
         .arg("-y");
     command.arg(outputfile);
 
@@ -321,6 +200,38 @@ fn cat_files<P: AsRef<Path> + AsRef<OsStr>>(inputfiles: Vec<PathBuf>, outputname
     }
 }
 
+fn get_fps(input: &str) -> i32 {
+    let mut command = Command::new(FFPROBE_PATH);
+    command.arg("-v").arg("0")
+        .arg("-of").arg("csv=p=0")
+        .arg("-select_streams").arg("v:0")
+        .arg("-show_entries").arg("stream=r_frame_rate")
+        .arg(input);
+    println!("{:?}", command);
+    let output = command
+        .output()
+        .expect("failed to execute ffprobe");
+
+    if ! output.status.success() {
+        let stderr_str = String::from_utf8_lossy(&*output.stderr).to_string();
+        panic!("Failed to run ffprobe.  Stderr follows.\n{}",
+               stderr_str);
+    }
+
+    let fpsstr = String::from_utf8_lossy(&*output.stdout).trim().to_string();
+    match fpsstr.find('/') {
+        None => {
+            println!("Could not determine FPS, using 30.");
+            return 30;
+        },
+        Some(i) => {
+            let numerator = &fpsstr[..i].parse::<i32>().unwrap();
+            let denominator = &fpsstr[i+1..].parse::<i32>().unwrap();
+            return (*numerator as f32 / *denominator as f32).round() as i32;
+        }
+    }
+}
+
 fn main() {
     let mut args = env::args();
 
@@ -328,14 +239,7 @@ fn main() {
 
     let mut input_file = args.next().expect(USAGE);
     let title = args.next().expect(USAGE);
-    let resolution = args.next().expect(USAGE);
-
-    let vp9params = if &*resolution == "1920" { VP9_PARAMS_1920_1080 }
-    else if &*resolution == "1280" { VP9_PARAMS_1280_720 }
-    else if &*resolution == "1024" { VP9_PARAMS_1024_576 }
-    else if &*resolution == "854" { VP9_PARAMS_854_480 }
-    else if &*resolution == "640" { VP9_PARAMS_640_360 }
-    else {  panic!(USAGE) };
+    let xres = args.next().expect(USAGE).parse::<i32>().unwrap();
 
     // Concat all mp4 files in current directory if inputfile was "."
     if input_file == "." {
@@ -345,11 +249,14 @@ fn main() {
         input_file = "concatenated.mp4".to_owned();
     }
 
+    println!("Extracting FPS...");
+    let fps = get_fps(&input_file);
+
     println!("Analyzing loudnorm...");
     let loudnorm = Loudnorm::from_analyze(&input_file);
 
     println!("Converting video (two passes w/ multiple functions)...");
-    convert(&input_file, "tmp1.webm", &loudnorm, vp9params);
+    convert(&input_file, "tmp1.webm", &loudnorm, xres, fps);
 
     let output_name = title
         .replace("/", "-")
