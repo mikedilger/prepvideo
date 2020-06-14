9957bb8524e7b2cb8100cf6279670a384a612328
Mike Dilger <mike@optcomp.nz>
2020-06-14 15:30:31 +1200

    reorder functions

--- src/main.rs
+++ src/main.rs
@@ -37,127 +37,40 @@ const USAGE: &'static str = "USAGE: prepvideo <inputfile> <title> <x-resolution>
 // https://web.archive.org/web/20200117200622/https://developers.google.com/media/vp9/settings/vod/
 // https://developers.google.com/media/vp9/settings/vod/
 
-fn args_shrink<'a>(command: &mut Command, size: &str) {
-    command
-        .arg("-vf")
-        .arg(&*format!("scale={}", size));
-}
+fn main() {
+    let mut args = env::args();
 
-fn args_vp9<'a>(command: &mut Command, xres: i32, yres: i32, fps: i32, level: &str) {
-    let compression_factor = match level {
-        "google" => 640, // 640 is near average of google recommendations
-        "good" => 1500, // I cannot tell the difference between this and "google"
-        "ok" => 3000, // Fast moving stuff (eye blinks) look a bit wrong, but otherwise good
-        _ => 1500,
-    };
+    let _bin = args.next().expect(USAGE);
 
-    let uncompressed_bitrate = 24 * fps * xres * yres;
+    let mut input_file = args.next().expect(USAGE);
+    let title = args.next().expect(USAGE);
+    let xres = args.next().expect(USAGE).parse::<i32>().unwrap();
+    let level = args.next().expect(USAGE);
 
-    let vp9_bitrate = uncompressed_bitrate / compression_factor;
-    println!("VP9 bitrate will be {}kbps", vp9_bitrate / 1000);
-
-    let tile_columns = if xres < 640 { 0 }
-    else if xres < 1024 { 1 }
-    else if xres < 2560 { 2 }
-    else { 3 };
-
-    let threads = 16; // for me, always use 16 threads
-    let crf = 31; // 31 is always reasonable
-
-    command
-        .arg("-b:v").arg(&*format!("{}", vp9_bitrate))
-        .arg("-minrate").arg(&*format!("{}", vp9_bitrate * 50 / 100))
-        .arg("-maxrate").arg(&*format!("{}", vp9_bitrate * 145 / 100))
-        .arg("-tile-columns").arg(&*format!("{}", tile_columns))
-        .arg("-g").arg("240")        // keyframe spacing
-        .arg("-threads").arg(&*format!("{}", threads))
-        .arg("-quality").arg("good")
-        .arg("-crf").arg(&*format!("{}", crf))
-        .arg("-c:v").arg("libvpx-vp9")
-        .arg("-c:a").arg("libopus");
-}
-
-fn convert(input: &str, outputfile: &str, loudnorm: &Loudnorm, xres: i32, fps: i32, level: &str) {
-
-    // We always use 1:1.777777 aspect ratios
-    let yres = xres * 5625 / 10000;
-
-    let pass1speed = 4; // at all resolutions
-    let pass2speed = if xres < 1024 { 1 } else { 2 };
-
-    // Pass 1
-    let mut command = Command::new(CPULIMIT_PATH);
-    command.arg("-l").arg(CPULIMIT)
-        .arg(FFMPEG_PATH)
-        .arg("-i").arg(input); // skip loudnorm on pass1
-    args_shrink(&mut command, &*format!("{}x{}", xres, yres));
-    args_vp9(&mut command, xres, yres, fps, level);
-    command.arg("-pass").arg("1")
-        .arg("-speed").arg(&*format!("{}", pass1speed))
-        .arg("-y");
-    command.arg(outputfile);
-
-    println!("{:?}", command);
-
-    let output = command
-        .output()
-        .expect("failed to execute ffmpeg");
-
-    if ! output.status.success() {
-        let stderr_str = String::from_utf8_lossy(&*output.stderr).to_string();
-        panic!("Failed to run ffmpeg multi command.  Stderr follows.\n{}",
-               stderr_str);
+    // Concat all mp4 files in current directory if inputfile was "."
+    if input_file == "." {
+        println!("Concatenating...");
+        let files = cwd_files_with_extension("mp4");
+        cat_files(files, "concatenated.mp4");
+        input_file = "concatenated.mp4".to_owned();
     }
 
-    // Pass 2
-    let mut command = Command::new(CPULIMIT_PATH);
-    command.arg("-l").arg(CPULIMIT)
-        .arg(FFMPEG_PATH)
-        .arg("-i").arg(input)
-        .arg("-af")
-        .arg(&*loudnorm.convert_af());
-    args_shrink(&mut command, &*format!("{}x{}", xres, yres));
-    args_vp9(&mut command, xres, yres, fps, level);
-    command.arg("-pass").arg("2")
-        .arg("-speed").arg(&*format!("{}", pass2speed))
-        .arg("-y");
-    command.arg(outputfile);
+    println!("Extracting FPS...");
+    let fps = get_fps(&input_file);
 
-    println!("{:?}", command);
+    println!("Analyzing loudnorm...");
+    let loudnorm = Loudnorm::from_analyze(&input_file);
 
-    let output = command
-        .output()
-        .expect("failed to execute ffmpeg");
+    println!("Converting video (two passes w/ multiple functions)...");
+    convert(&input_file, "tmp1.webm", &loudnorm, xres, fps, &*level);
 
-    if ! output.status.success() {
-        let stderr_str = String::from_utf8_lossy(&*output.stderr).to_string();
-        panic!("Failed to run ffmpeg multi command.  Stderr follows.\n{}",
-               stderr_str);
-    }
-}
+    let output_name = title
+        .replace("/", "-")
+        .replace(" ", "_")
+        .to_string();
 
-fn strip_metadata(input: &str, output: &str, title: &str)
-{
-    let mut command = Command::new(FFMPEG_PATH);
-    command
-        .arg("-y") // overwrite output files w/o asking
-        .arg("-i").arg(input)
-        .arg("-c").arg("copy")
-        .arg("-map_metadata").arg("-1")
-        .arg("-metadata").arg(format!("title={}",title))
-        .arg(output);
-
-    println!("{:?}", command);
-
-    let output = command
-        .output()
-        .expect("failed to execute ffmpeg");
-
-    if ! output.status.success() {
-        let stderr_str = String::from_utf8_lossy(&*output.stderr).to_string();
-        panic!("Failed to run ffmpeg to strip metadata.  Stderr follows.\n{}",
-               stderr_str);
-    }
+    println!("Stripping metadata...");
+    strip_metadata("tmp1.webm", &*format!("{}.webm", output_name), &title);
 }
 
 fn cwd_files_with_extension(ext: &str) -> Vec<PathBuf> {
@@ -240,38 +153,125 @@ fn get_fps(input: &str) -> i32 {
     }
 }
 
-fn main() {
-    let mut args = env::args();
+fn convert(input: &str, outputfile: &str, loudnorm: &Loudnorm, xres: i32, fps: i32, level: &str) {
 
-    let _bin = args.next().expect(USAGE);
+    // We always use 1:1.777777 aspect ratios
+    let yres = xres * 5625 / 10000;
 
-    let mut input_file = args.next().expect(USAGE);
-    let title = args.next().expect(USAGE);
-    let xres = args.next().expect(USAGE).parse::<i32>().unwrap();
-    let level = args.next().expect(USAGE);
+    let pass1speed = 4; // at all resolutions
+    let pass2speed = if xres < 1024 { 1 } else { 2 };
 
-    // Concat all mp4 files in current directory if inputfile was "."
-    if input_file == "." {
-        println!("Concatenating...");
-        let files = cwd_files_with_extension("mp4");
-        cat_files(files, "concatenated.mp4");
-        input_file = "concatenated.mp4".to_owned();
+    // Pass 1
+    let mut command = Command::new(CPULIMIT_PATH);
+    command.arg("-l").arg(CPULIMIT)
+        .arg(FFMPEG_PATH)
+        .arg("-i").arg(input); // skip loudnorm on pass1
+    args_shrink(&mut command, &*format!("{}x{}", xres, yres));
+    args_vp9(&mut command, xres, yres, fps, level);
+    command.arg("-pass").arg("1")
+        .arg("-speed").arg(&*format!("{}", pass1speed))
+        .arg("-y");
+    command.arg(outputfile);
+
+    println!("{:?}", command);
+
+    let output = command
+        .output()
+        .expect("failed to execute ffmpeg");
+
+    if ! output.status.success() {
+        let stderr_str = String::from_utf8_lossy(&*output.stderr).to_string();
+        panic!("Failed to run ffmpeg multi command.  Stderr follows.\n{}",
+               stderr_str);
     }
 
-    println!("Extracting FPS...");
-    let fps = get_fps(&input_file);
+    // Pass 2
+    let mut command = Command::new(CPULIMIT_PATH);
+    command.arg("-l").arg(CPULIMIT)
+        .arg(FFMPEG_PATH)
+        .arg("-i").arg(input)
+        .arg("-af")
+        .arg(&*loudnorm.convert_af());
+    args_shrink(&mut command, &*format!("{}x{}", xres, yres));
+    args_vp9(&mut command, xres, yres, fps, level);
+    command.arg("-pass").arg("2")
+        .arg("-speed").arg(&*format!("{}", pass2speed))
+        .arg("-y");
+    command.arg(outputfile);
 
-    println!("Analyzing loudnorm...");
-    let loudnorm = Loudnorm::from_analyze(&input_file);
+    println!("{:?}", command);
 
-    println!("Converting video (two passes w/ multiple functions)...");
-    convert(&input_file, "tmp1.webm", &loudnorm, xres, fps, &*level);
+    let output = command
+        .output()
+        .expect("failed to execute ffmpeg");
 
-    let output_name = title
-        .replace("/", "-")
-        .replace(" ", "_")
-        .to_string();
+    if ! output.status.success() {
+        let stderr_str = String::from_utf8_lossy(&*output.stderr).to_string();
+        panic!("Failed to run ffmpeg multi command.  Stderr follows.\n{}",
+               stderr_str);
+    }
+}
 
-    println!("Stripping metadata...");
-    strip_metadata("tmp1.webm", &*format!("{}.webm", output_name), &title);
+fn args_shrink<'a>(command: &mut Command, size: &str) {
+    command
+        .arg("-vf")
+        .arg(&*format!("scale={}", size));
+}
+
+fn args_vp9<'a>(command: &mut Command, xres: i32, yres: i32, fps: i32, level: &str) {
+    let compression_factor = match level {
+        "google" => 640, // 640 is near average of google recommendations
+        "good" => 1500, // I cannot tell the difference between this and "google"
+        "ok" => 3000, // Fast moving stuff (eye blinks) look a bit wrong, but otherwise good
+        _ => 1500,
+    };
+
+    let uncompressed_bitrate = 24 * fps * xres * yres;
+
+    let vp9_bitrate = uncompressed_bitrate / compression_factor;
+    println!("VP9 bitrate will be {}kbps", vp9_bitrate / 1000);
+
+    let tile_columns = if xres < 640 { 0 }
+    else if xres < 1024 { 1 }
+    else if xres < 2560 { 2 }
+    else { 3 };
+
+    let threads = 16; // for me, always use 16 threads
+    let crf = 31; // 31 is always reasonable
+
+    command
+        .arg("-b:v").arg(&*format!("{}", vp9_bitrate))
+        .arg("-minrate").arg(&*format!("{}", vp9_bitrate * 50 / 100))
+        .arg("-maxrate").arg(&*format!("{}", vp9_bitrate * 145 / 100))
+        .arg("-tile-columns").arg(&*format!("{}", tile_columns))
+        .arg("-g").arg("240")        // keyframe spacing
+        .arg("-threads").arg(&*format!("{}", threads))
+        .arg("-quality").arg("good")
+        .arg("-crf").arg(&*format!("{}", crf))
+        .arg("-c:v").arg("libvpx-vp9")
+        .arg("-c:a").arg("libopus");
+}
+
+fn strip_metadata(input: &str, output: &str, title: &str)
+{
+    let mut command = Command::new(FFMPEG_PATH);
+    command
+        .arg("-y") // overwrite output files w/o asking
+        .arg("-i").arg(input)
+        .arg("-c").arg("copy")
+        .arg("-map_metadata").arg("-1")
+        .arg("-metadata").arg(format!("title={}",title))
+        .arg(output);
+
+    println!("{:?}", command);
+
+    let output = command
+        .output()
+        .expect("failed to execute ffmpeg");
+
+    if ! output.status.success() {
+        let stderr_str = String::from_utf8_lossy(&*output.stderr).to_string();
+        panic!("Failed to run ffmpeg to strip metadata.  Stderr follows.\n{}",
+               stderr_str);
+    }
 }
